a249939b1e98ec3965d69e3212d8df821e6fc4be
Raymond <raymond.rizzo@gmail.com>
2017-04-12 17:00:27 -0400

    Initial release.

--- file-metadata-ms.js
+++ file-metadata-ms.js
@@ -1,1 +1,24 @@
+/* 
+File Metadata Microservice
+Raymond Rizzo
+*/
 
+var express = require('express');
+var multer = require('multer');
+var bodyParser = require('body-parser');
+var fs = require('fs');
+var app = express();
+
+app.post('/Upload',multer({ dest: './uploads/'}).single('uploadFile'), function(httpRequest, httpResponse){
+	var storage = multer.memoryStorage();
+	var upload = multer({ storage: storage });
+	var sizeOut = { 'size': httpRequest.file.size};
+	httpResponse.writeHead(200, { "Content-Type": "application/json" });
+	httpResponse.end(JSON.stringify(sizeOut));
+});
+
+app.get('/*', function(httpRequest, httpResponse){
+	httpResponse.sendFile(__dirname + '/about.html');
+});
+
+app.listen(process.env.PORT);
